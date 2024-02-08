cf477a537e4d0a4b2ee7ad0027e985db1c7118ae
JuanHeis <juaany98@gmail.com>
2024-02-08 19:02:35 -0300

    change script url to 2-8-24

--- javascript/fetchUrl.js
+++ javascript/fetchUrl.js
@@ -1,3 +1,3 @@
 function getScriptUrl() {
-  return 'https://script.google.com/macros/s/AKfycbxkz2hMrSYpm8NVfxiQkh7KDskHJHmAZhdC-fdvUiZUZpjGP9VU_VYHrmVSNimlxF4H/exec';
+  return 'https://script.google.com/macros/s/AKfycbxyTNy8x2REm6p5W6_j6yHu6SBYpe_QIGMruSjavYWouP8gGtdqObhqY412DTJuH3UM/exec';
 }
