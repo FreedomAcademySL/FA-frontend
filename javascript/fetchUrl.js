13ae4e6c84aed6ff9141ffab063f2a854a9d8302
Christian <christian.coriasco@halopowered.com>
2024-02-07 22:17:08 -0300

    Change script url

--- javascript/fetchUrl.js
+++ javascript/fetchUrl.js
@@ -1,5 +1,3 @@
 function getScriptUrl() {
-  return 'https://script.google.com/macros/s/AKfycbypu6lUAL0JsgH5wnugvJq1eJwmzJjgvCYjT6blfQveO0gW9r0RwE4Z10Meg6uP8Rq5/exec';
+  return 'https://script.google.com/macros/s/AKfycbxkz2hMrSYpm8NVfxiQkh7KDskHJHmAZhdC-fdvUiZUZpjGP9VU_VYHrmVSNimlxF4H/exec';
 }
-
-// return 'https://script.google.com/macros/s/AKfycbwmADwSlEO-Pu7puk1icrLYj7c0_LnxviFR6Sl9nemhuSIxJhH8QNJDLAZt9AvUM7H1/exec';
